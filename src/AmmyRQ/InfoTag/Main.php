2b5e9700d2e25431c022bd8654c7cded6e4f4652
Amélie Riva <63977585+AmmyR@users.noreply.github.com>
2021-09-25 22:32:16 -0500

    Update Main.php
    - Add factions support
    - Move verifyFile() method to API file
    - Reset player's nametag when changing world

--- src/AmmyRQ/InfoTag/Main.php
+++ src/AmmyRQ/InfoTag/Main.php
@@ -2,19 +2,20 @@
 
 namespace AmmyRQ\InfoTag;
 
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\plugin\PluginException;
 use pocketmine\utils\Config;
 
 use AmmyRQ\InfoTag\{API, UpdateNametagTask};
+use AmmyRQ\InfoTag\Factions\FactionsManager;
 
 class Main extends PluginBase implements Listener
 {
-    /** @const float */
-    private const CFG_VERSION = 1.0;
 
     /** @var bool */
     public static bool $isPureChatEnabled = false;
@@ -39,10 +40,13 @@ class Main extends PluginBase implements Listener
     public function onEnable() : void
     {
         self::$instance = $this;
-        $this->verifyFile();
+        API::verifyFile();
 
         //Starts the nametag update task
         new UpdateNametagTask();
+        
+        //Initialises factions manager
+        FactionsManager::init();
 
         $pluginManager = $this->getServer()->getPluginManager();
 
@@ -57,43 +61,6 @@ class Main extends PluginBase implements Listener
     }
 
     /**
-     * Verifies the existence, version & content of the file "format.yml"
-     * @return void
-     */
-    public function verifyFile() : void
-    {
-        if(!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder());
-
-        if(!is_file($this->getDataFolder() . "format.yml")) $this->saveResource("format.yml");
-
-        $file = new Config($this->getDataFolder() . "format.yml", Config::YAML);
-
-        if(!$file->exists("cfg-version") || $file->get("cfg-version") !== self::CFG_VERSION)
-        {
-            $this->getServer()->getLogger()->debug("[InfoTag] The configuration version does not exists or is out of date in the format.yml file. Updating file...");
-
-            @unlink($this->getDataFolder() . "format.yml");
-            $this->saveResource("format.yml");
-        }
-
-        if(!$file->exists("format"))
-        {
-            $this->getServer()->getLogger()->debug("[InfoTag] The content in the format.yml file does not exists or it is incomplete (\"format\" key). Updating file...");
-
-            @unlink($this->getDataFolder() . "format.yml");
-            $this->saveResource("format.yml");
-        }
-
-        if(!$file->exists("worlds") || is_null($file->get("worlds")))
-        {
-            $this->getServer()->getLogger()->debug("[InfoTag] The content in the format.yml file does not exists or it is incomplete (\"world\" key). Updating file...");
-
-            @unlink($this->getDataFolder() . "format.yml");
-            $this->saveResource("format.yml");
-        }
-    }
-
-    /**
      * Records the player's name and device identifier in an array
      * @see API::$playerDevices
      * @param DataPacketReceiveEvent $event
@@ -103,7 +70,7 @@ class Main extends PluginBase implements Listener
     {
         if($event->getPacket() instanceof LoginPacket)
         {
-            $file = new Config(Main::getInstance()->getDataFolder() . "format.yml", Config::YAML);
+            $file = new Config(self::getInstance()->getDataFolder() . "format.yml", Config::YAML);
 
             //Checks if {device} exists in the format.yml file
             if(strpos($file->get("format"), "{device}"))
@@ -113,6 +80,22 @@ class Main extends PluginBase implements Listener
                 if(array_key_exists($name, API::$playerDevices)) unset(API::$playerDevices[$name]);
                 API::$playerDevices[$name] = $event->getPacket()->clientData["DeviceOS"];
             }
+        }
+    }
+
+    /**
+     * Resets the player's nametag format if the player travels to a world where the info nametag is not allowed
+     * @param EntityLevelChangeEvent $event
+     * @return void
+     */
+    public function onEntityLevelChangeEvent(EntityLevelChangeEvent $event) : void
+    {
+        $player = $event->getEntity();
+
+        if($player instanceof Player)
+        {
+            if(!in_array($event->getTarget()->getName(), API::getAllowedWorlds()))
+                API::resetNametag($player);
         }
     }
 }
